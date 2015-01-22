a6d588a38c8c23101a8836c8dbd26ae8e3f2176e
Sunil Gopinath <sunlchk@users.noreply.github.com>
2015-01-22 19:46:52 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+# This merges the training and the test sets to create a single data set.
+tmp1 <- read.table("train/X_train.txt")
+tmp2 <- read.table("test/X_test.txt")
+X <- rbind(tmp1, tmp2)
+tmp1 <- read.table("train/subject_train.txt")
+tmp2 <- read.table("test/subject_test.txt")
+S <- rbind(tmp1, tmp2)
+tmp1 <- read.table("train/y_train.txt")
+tmp2 <- read.table("test/y_test.txt")
+Y <- rbind(tmp1, tmp2)
+# This extracts the measurements on the mean and standard deviation for each one.
+features <- read.table("features.txt")
+indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, indices_of_good_features]
+names(X) <- features[indices_of_good_features, 2]
+names(X) <- gsub("\\(|\\)", "", names(X))
+names(X) <- tolower(names(X))
+# This uses descriptive activity names to name the activities in the data set.
+activities <- read.table("activity_labels.txt")
+activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+Y[,1] = activities[Y[,1], 2]
+names(Y) <- "activity"
+# This labels the data set with descriptive activity names.
+names(S) <- "subject"
+cleaned <- cbind(S, Y, X)
+write.table(cleaned, "mergedCleanData.txt")
+# This creates a 2nd independent tidy data set with average of each variable for each activity & each subject.
+uniqueSubjects = unique(S)[,1]
+numSubjects = length(unique(S)[,1])
+numActivities = length(activities[,1])
+numCols = dim(cleaned)[2]
+result = cleaned[1:(numSubjects*numActivities), ]
+row = 1
+for (s in 1:numSubjects) {
+  for (a in 1:numActivities) {
+  result[row, 1] = uniqueSubjects[s]
+  result[row, 2] = activities[a, 2]
+  tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
+  result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+  row = row+1
+  }
+}
+write.table(result, "datasetWithTheAverages.txt",row.name=FALSE)
+
